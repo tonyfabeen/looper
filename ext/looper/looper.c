fcd15e3b24a47a681593dcc5fe6f94bcc518d2af
Tony Fabeen <tony.fabeen@gmail.com>
2013-10-14 14:14:32 -0300

    Taking off unused stuff.

--- ext/looper/looper.c
+++ ext/looper/looper.c
@@ -114,7 +114,7 @@ static VALUE tcp_server_new(VALUE tcp_server_class){
   VALUE tcp_server_data = Data_Wrap_Struct(tcp_server_class, 0, free, server);
   rb_obj_call_init(tcp_server_data, 0, NULL);
 
-  return uv_run(loop, UV_RUN_DEFAULT);
+  return tcp_server_class;
 }
 
 //TCPServer.initialize implementation
@@ -124,40 +124,12 @@ static VALUE tcp_serve_init(VALUE self){
   tcp_server_start();
 
   return uv_run(loop, UV_RUN_DEFAULT);
-  fprintf(stdout, "[Looper Ext] TCP Server initialized\n");
-  return self;
-}
-
-//Initialize EventLoop class
-static VALUE eventloop_start(VALUE eventloop_class){
-  //uv_loop_t *loop = uv_loop_new();
-  uv_loop_t *loop = uv_default_loop();
-
-  if(!loop){
-    fprintf(stderr, "[Looper Ext] Error on start LibUV\n");
-  }
-
-  VALUE eventloop_data = Data_Wrap_Struct(eventloop_class, 0, NULL, loop);
-  rb_obj_call_init(eventloop_data, 0,NULL);
-
-
-  return eventloop_data;
-}
-
-//EventLoop.initialize implementation
-static VALUE eventloop_init(VALUE self){
-  return self;
 }
 
 
 void Init_looper(){
   mLooper = rb_define_module("Looper");
   mTCPServer = rb_define_module_under(mLooper, "TCPServer");
-  cEventLoop = rb_define_class_under(mLooper, "EventLoop", rb_cObject);
-
-  //Initialize EventLoop class
-  rb_define_singleton_method(cEventLoop, "start", eventloop_start,0);
-  rb_define_method(cEventLoop, "initialize", eventloop_init, 0);
 
   //Initialize TCPServer Module
   rb_define_singleton_method(mTCPServer, "new", tcp_server_new, 0);
