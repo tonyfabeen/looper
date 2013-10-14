d941785c7c420d080ad8257d6e189ceea8a554fb
Tony Fabeen <tony.fabeen@gmail.com>
2013-10-14 15:53:58 -0300

    * Reading Buffer and echoing it on Socket.

--- ext/looper/looper.c
+++ ext/looper/looper.c
@@ -33,14 +33,29 @@
 static uv_loop_t *loop;
 static uv_handle_t *server;
 static uv_tcp_t tcpServer;
+
 typedef struct {
   uv_write_t req;
   uv_buf_t buf;
 } write_req_t;
 int server_closed;
+
 static void on_connection(uv_stream_t*, int status);
 
-//Ruby Stuff
+static void alloc_buffer(uv_handle_t *handle,
+    size_t suggested_size,
+    uv_buf_t *buffer);
+
+static void read_buffer(uv_stream_t *handle,
+    ssize_t nread,
+    const uv_buf_t *buf);
+
+static void write_buffer(uv_write_t *req, int status);
+static void after_shutdown(uv_shutdown_t *req, int status);
+static void on_close(uv_handle_t *peer);
+static void on_server_close(uv_handle_t *handle);
+
+///Ruby Stuff
 static VALUE mLooper;
 static VALUE cEventLoop;
 static VALUE mTCPServer;
@@ -72,7 +87,104 @@ static void on_connection(uv_stream_t *server, int status){
   if(rb_respond_to(cTarget, rb_intern("on_connection"))){
     rb_funcall(cTarget, rb_intern("on_connection"),0,0);
   }
-  //r = uv_read_start(stream, alloc_buffer, after_read);
+
+  r = uv_read_start(stream, alloc_buffer, read_buffer);
+}
+
+static void alloc_buffer(uv_handle_t *handle,
+    size_t suggested_size,
+    uv_buf_t *buffer){
+
+  buffer->base = malloc(suggested_size);
+  buffer->len  = suggested_size;
+}
+
+static void read_buffer(uv_stream_t *handle,
+    ssize_t nread,
+    const uv_buf_t *buf){
+
+  int i;
+  write_req_t *wr;
+  uv_shutdown_t *req;
+
+  if(nread < 0){
+    ASSERT(nread == UV_EOF);
+
+    if(buf->base){
+      free(buf->base);
+    }
+
+    req = (uv_shutdown_t *) malloc(sizeof *req);
+    uv_shutdown(req, handle, after_shutdown);
+
+    return;
+  }
+
+  if(nread == 0){
+    /* nothing to read */
+    free(buf->base);
+    return;
+  }
+
+  /*
+   * 'Q' quits the server
+   * 'QS' closes the stream
+   */
+  if(!server_closed){
+
+    for(i = 0; i < nread; i++){
+      if(buf->base[i] == 'Q'){
+        if(i + 1 < nread && buf->base[i + 1] == 'S'){
+          free(buf->base);
+          uv_close((uv_handle_t *) handle, on_close);
+          return;
+        }else{
+          uv_close(server, on_server_close);
+          server_closed = 1;
+        }
+      }
+    }
+  }
+
+  wr = (write_req_t *) malloc(sizeof *wr);
+  wr->buf = uv_buf_init(buf->base, nread);
+
+  if(uv_write(&wr->req, handle, &wr->buf, 1, write_buffer)){
+    LOG_ERROR("uv_write failed");
+  }
+}
+
+static void write_buffer(uv_write_t *req, int status){
+  write_req_t *wr;
+
+  /*free read/write buffer and request*/
+  wr = (write_req_t *) req;
+  free(wr->buf.base);
+  free(wr);
+
+  if(status == 0)
+    return;
+
+  LOG_UV_ERROR("uv_write error: ", uv_strerror(status));
+
+  if(status == UV_ECANCELED)
+    return;
+
+  ASSERT(status == UV_EPIPE);
+  uv_close((uv_handle_t *) req->handle, on_close);
+}
+
+
+static void after_shutdown(uv_shutdown_t *req, int status){
+  uv_close((uv_handle_t *) req->handle, on_close);
+}
+
+static void on_close(uv_handle_t *peer){
+  free(peer);
+}
+
+static void on_server_close(uv_handle_t *handle){
+  ASSERT(handle == server);
 }
 
 
