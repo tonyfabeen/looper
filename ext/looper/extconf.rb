3f1f51480eecc6a43a4ef65a11c844d1f44c92aa
Tony Fabeen <tony.fabeen@gmail.com>
2013-10-16 16:36:43 -0300

    Libuv now lives in ext/deps/libuv

--- ext/looper/extconf.rb
+++ ext/looper/extconf.rb
@@ -1,4 +1,10 @@
 require 'mkmf'
 
+`cd ../deps/libuv; sh autogen.sh; sh configure; make; cd ../..`
+
+dir_config('uv', File.expand_path('../deps/libuv/include'),
+                   File.expand_path('../deps/libuv/.libs'))
+
 have_library('uv')
+
 create_makefile('looper')
