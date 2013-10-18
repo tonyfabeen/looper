629fc0e273c67740322eca9bc43e10d8191b38e2
Tony Fabeen <tony.fabeen@gmail.com>
2013-10-18 11:55:59 -0300

    Add build steps

--- ext/looper/extconf.rb
+++ ext/looper/extconf.rb
@@ -1,11 +1,11 @@
 require 'mkmf'
 
 #$CFLAGS = CONFIG['CFLAGS'] = '-shared -fPIC'
-
-`cd ../deps/libuv; sh autogen.sh; sh configure; make; cd ../..`
+find_executable('make')
+system('cd ../.. && make build_libuv')
 
 dir_config('uv', File.expand_path('../deps/libuv/include'),
-                   File.expand_path('../deps/libuv/.libs'))
+                 File.expand_path('../deps/libuv/.libs'))
 
 have_library('uv')
 
