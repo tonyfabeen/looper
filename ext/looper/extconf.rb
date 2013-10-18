044cf941c12f93e54b8dfbd677513b8bbcd72703
Tony Fabeen <tony.fabeen@gmail.com>
2013-10-18 13:35:32 -0300

    export ldlibrarypath to take libs

--- ext/looper/extconf.rb
+++ ext/looper/extconf.rb
@@ -3,6 +3,7 @@ require 'mkmf'
 #$CFLAGS = CONFIG['CFLAGS'] = '-shared -fPIC'
 find_executable('make')
 system('cd ../.. && make build_libuv')
+system('export LD_LIBRARY_PATH=/var/lib/gems/1.9.1/gems/looper-0.0.1/ext/deps/libuv/.libs')
 
 dir_config('uv', File.expand_path('../deps/libuv/include'),
                  File.expand_path('../deps/libuv/.libs'))
